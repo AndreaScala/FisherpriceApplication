af379a69f173aeb367e789d38735f8c29a5f85f7
Skylarius <ilario.gerloni@gmail.com>
2018-02-14 18:01:58 +0100

    Aggiustato EJB SIIIIII!!!!!

--- FisherpriseApplication/FisherpriseApplication-war/src/java/REST/RestAPI.java
+++ FisherpriseApplication/FisherpriseApplication-war/src/java/REST/RestAPI.java
@@ -1,19 +1,34 @@
 package REST;
 
 import EJB.WriteBeanLocal;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.EJB;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import utilities.LogEntry;
 
 @Path("/datamanager")
 public class RestAPI {
+
+    WriteBeanLocal writeBean = lookupWriteBeanLocal();
     
-    @EJB
-    private static WriteBeanLocal wbl;
     
     @POST
     public String receiveMessage(){
-        return wbl.write("Ciao");
+        return writeBean.write("Ciao");
+    }
+
+    private WriteBeanLocal lookupWriteBeanLocal() {
+        try {
+            Context c = new InitialContext();
+            return (WriteBeanLocal) c.lookup("java:global/FisherpriseApplication/FisherpriseApplication-ejb/WriteBean!EJB.WriteBeanLocal");
+        } catch (NamingException ne) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
+            throw new RuntimeException(ne);
+        }
     }
 }
